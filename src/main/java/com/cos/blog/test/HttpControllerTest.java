9c66b08dce0234a8d0b9390868b9955ac6ab9067
sa46lll <9812smh@naver.com>
2021-11-13 12:53:30 +0900

    http get, post, put 설정

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,20 +18,20 @@ public class HttpControllerTest {
 	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) { //@RequestParam int id, @RequestParam String username
+		return "get 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail(); //id=1&username=user&password=1234&email=user@gmail.com
 	}
 	
 	// http://localhost:8080/http/post (insert)
-	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	@PostMapping("/http/post") // text/plain, application/json
+	public String postTest(@RequestBody Member m) { // MessageConverter (스프링부트)
+		return "post 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
 	// http://localhost:8080/http/put (update)
 	@PutMapping("/http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
 	// http://localhost:8080/http/delete (delete)
